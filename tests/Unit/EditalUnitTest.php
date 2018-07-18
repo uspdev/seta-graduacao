cffff46a5006f18925d2b3fbbb6c86faeaf7c6c0
Fabio Jose Moretti <fabioj.moretti@gmail.com>
2018-07-18 15:03:56 -0300

    Alterado nome da funcao de teste unitario do edital

--- tests/Unit/EditalUnitTest.php
+++ tests/Unit/EditalUnitTest.php
@@ -15,7 +15,7 @@ class EditalUnitTest extends TestCase
      * @test
      * @return void
      */
-    public function deveriaRetornarUmEdital()
+    public function deveriaRetornarEditalDoAnoSolicitado()
     {
         factory(\App\Edital::class)->states('year_ago')->create();
         factory(\App\Edital::class)->create();
