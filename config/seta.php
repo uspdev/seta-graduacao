2685b36920fe19b528ec93be247a56df47cef60f
Fabio Jose Moretti <fabioj.moretti@gmail.com>
2018-07-19 16:17:42 -0300

    Adicionando configuração custom
    Agrupando configuração do seta em arquivo próprio

---
+++ config/seta.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Configuração dos administradores do sistema
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the database connections below you wish
+    | to use as your default connection for all database work. Of course
+    | you may use many connections at once using the Database library.
+    |
+ */
+    'admins' => env('CODPES_ADMINS', false)
+];
