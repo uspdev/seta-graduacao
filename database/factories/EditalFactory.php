95261a6ec2316c48ec54cacca2dcfface4b8d217
Fabio Jose Moretti <fabioj.moretti@gmail.com>
2018-07-20 11:51:41 -0300

    Adicionado o state active a factory do edital

--- database/factories/EditalFactory.php
+++ database/factories/EditalFactory.php
@@ -47,3 +47,27 @@ $factory->state(App\Edital::class, 'year_ago', function (Faker $faker) {
         'dtFinalApresentacaoTCC' => $now->addMonths(2)->toDateString(),
     ];
 });
+
+$factory->state(App\Edital::class, 'unactive', function (Faker $faker) {
+    $now = Carbon::now();
+    $now->subYear(2);
+
+    $publicacao_resultados = Carbon::now()->addMonths(1)->addWeek(1)->subYear();
+    $final_relatorio = Carbon::now()->addMonths(7)->addWeek(1)->subYear()->toDateString();
+    // dump($publicacao_resultados);
+    return [
+        'anoReferencia' => $now->year,
+        'dtInicialInscricao' => $now->toDateString(),
+        'dtFinalInscricao' => $now->addMonths(1)->toDateString(),
+        'dtPublicacaoResultados' => $publicacao_resultados->toDateString(),
+        'dtInicialRelatorio' => $final_relatorio,
+        'dtFinalRelatorio' => $now->addMonths(1)->toDateString(),
+        'dtInicialTCC' => $publicacao_resultados->toDateString(),
+        'dtFinalTCC' => $now->addYear(1)->toDateString(),
+        'dtInicialInscricaoBanca' => $publicacao_resultados->toDateString(),
+        'dtFinalInscricaoBanca' => $now->toDateString(),
+        'dtInicialApresentacaoTCC' => $now->addWeek(1)->toDateString(),
+        'dtFinalApresentacaoTCC' => $now->addMonths(2)->toDateString(),
+        'ativo' => 0,
+    ];
+});
